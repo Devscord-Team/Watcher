c6d1e8fbf98e9ca32832c3c9d6cc96f5d6b1880c
Marcin99b <marcin.bigos99@gmail.com>
2025-12-11 17:00:22 +0100

    marchew message

--- src/Watcher.Runner/DiscordEventHandlers/MessageReceivedHandler.cs
+++ src/Watcher.Runner/DiscordEventHandlers/MessageReceivedHandler.cs
@@ -6,6 +6,8 @@ using Watcher.Runner.Storage;
 namespace Watcher.Runner.DiscordEventHandlers;
 public class MessageReceivedHandler(IEventLogger eventLogger, IMessagesStorage messagesStorage) : IDiscordEventHandler<SocketMessage>
 {
+    private const string MARCHEW_MESSAGE = "Moim zdaniem to nie ma tak, że coś jest programowaniem, albo nie jest programowaniem. Gdybym miał powiedzieć, co cenię w programowaniu najbardziej, powiedziałbym, że ludzi. Ekhm… Ludzi, którzy podali mi pomocną dokumentacje, kiedy sobie nie radziłem, kiedy byłem sam. I co ciekawe, to właśnie przypadkowe spotkania wpływają na nasze życie. Chodzi o to, że kiedy wyznaje się pewne wartości, nawet pozornie uniwersalne, bywa, że nie znajduje się zrozumienia, które by tak rzec, które pomaga się nam rozwijać. Ja miałem szczęście, by tak rzec, ponieważ je znalazłem. I dziękuję życiu. Dziękuję mu, życie to śpiew, życie to taniec, życie to miłość. Wielu ludzi pyta mnie o to samo, ale jak ty to robisz? Skąd czerpiesz tę radość? A ja odpowiadam, że to proste, to umiłowanie życia, to właśnie ono sprawia, że dzisiaj na przykład programuje nawigacje, a jutro… kto wie, dlaczego by nie, oddam się pracy społecznej i będę ot, choćby uczyć… znaczy… juniorów.";
+
     public async Task Handle(SocketMessage message)
     {
         var messageInfo = message.ToMessageInfo();
@@ -13,6 +15,10 @@ public class MessageReceivedHandler(IEventLogger eventLogger, IMessagesStorage m
         eventLogger.Event_ReceivedMessage(messageInfo);
         messagesStorage.SaveMessageInfo(messageInfo);
 
-        await Task.CompletedTask;
+        var normalized = message.Content.Trim().ToLower();
+        if (normalized is "-marchew" or "!marchew")
+        {
+            _ = await message.Channel.SendMessageAsync(MARCHEW_MESSAGE);
+        }
     }
 }
