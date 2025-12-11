2f9f44859428793eb56de590be7b8ca0810ef3c9
Marcin99b <marcin.bigos99@gmail.com>
2025-12-11 12:04:47 +0100

    experiments with slash command

--- src/Watcher.Runner/SlashCommandExecutedHandler.cs
+++ src/Watcher.Runner/SlashCommandExecutedHandler.cs
@@ -1,11 +1,17 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 
 namespace Watcher.Runner;
 
 public class SlashCommandExecutedHandler : IDiscordEventHandler<SocketSlashCommand>
 {
+    //experiments
     public async Task Handle(SocketSlashCommand message)
     {
-        await Task.CompletedTask;
+        var channel = message.Data.Options.First().Value as ISocketMessageChannel;
+
+        var messages = await channel!.GetMessagesAsync(10, CacheMode.AllowDownload).FlattenAsync();
+        var content = string.Join("\n", messages.Select(x => $"{x.Author.Username}: {x.Content}"));
+        await message.RespondAsync(content);
     }
 }
