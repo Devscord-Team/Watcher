a30b856f68b1d61d9c23a3b94b8924fa298c0b25
Marcin99b <marcin.bigos99@gmail.com>
2025-12-13 00:51:47 +0100

    AnomalyDetector

---
+++ src/Watcher.Runner/AnomalyDetector.cs
@@ -0,0 +1,14 @@
+﻿using Watcher.Runner.Storage;
+
+namespace Watcher.Runner;
+public class AnomalyDetector(IMessagesStorage storage)
+{
+    public async Task ScanChannel(ulong channelId)
+    {
+        //todo
+        //get statistics (standard activity per period, for example day of week, part of day etc)
+        //monitor current activity
+        //send event when current activity is higher than average
+        await Task.CompletedTask;
+    }
+}
