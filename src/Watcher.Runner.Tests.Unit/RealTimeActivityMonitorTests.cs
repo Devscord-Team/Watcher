2ca86d58f2003e8fddd385fa6905c8e858ad6ea0
Marcin99b <marcin.bigos99@gmail.com>
2025-12-15 18:22:42 +0100

    RealTimeActivityMonitorTests

---
+++ src/Watcher.Runner.Tests.Unit/RealTimeActivityMonitorTests.cs
@@ -0,0 +1,124 @@
+﻿using Moq;
+using Watcher.Runner.DiscordEventHandlers;
+using Watcher.Runner.Monitoring;
+using Watcher.Runner.Providers;
+using Watcher.Runner.Storage;
+
+namespace Watcher.Runner.Tests.Unit;
+
+[TestFixture]
+public class RealTimeActivityMonitorTests
+{
+    private Mock<IEventBus> _eventBusMock;
+    private Mock<IDateTimeProvider> _dateTimeProviderMock;
+    private Mock<IMessagesStorage> _messagesStorageMock;
+    private RealTimeActivityMonitor _sut;
+
+    private DateTime _now;
+    private Action<MessageInfoReceivedEvent>? _messageReceivedHandler;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._eventBusMock = new Mock<IEventBus>();
+        this._dateTimeProviderMock = new Mock<IDateTimeProvider>();
+        this._messagesStorageMock = new Mock<IMessagesStorage>();
+
+        this._now = new DateTime(2024, 6, 15, 12, 0, 0, DateTimeKind.Utc);
+        _ = this._dateTimeProviderMock.Setup(x => x.GetUtcNow()).Returns(() => this._now);
+        _ = this._messagesStorageMock.Setup(x => x.GetAllMessagesInfos()).Returns([]);
+
+        _ = this._eventBusMock
+            .Setup(x => x.Subscribe(It.IsAny<Action<MessageInfoReceivedEvent>>()))
+            .Callback<Action<MessageInfoReceivedEvent>>(h => this._messageReceivedHandler = h);
+
+        this._sut = new RealTimeActivityMonitor(
+            this._eventBusMock.Object,
+            this._dateTimeProviderMock.Object,
+            this._messagesStorageMock.Object);
+    }
+
+    private MessageInfo CreateMessage(DateTime sentAt) => new(ServerId: 1, ChannelId: 1, UserId: 1, MessageId: 1, SentAt: sentAt);
+
+    private void SimulateMessageReceived(DateTime sentAt) => this._messageReceivedHandler?.Invoke(new MessageInfoReceivedEvent(this.CreateMessage(sentAt)));
+
+    private void SetupStorageMessages(params DateTime[] sentAtTimes)
+    {
+        var messages = sentAtTimes.Select(this.CreateMessage).ToList();
+        this._messagesStorageMock.Setup(x => x.GetAllMessagesInfos()).Returns(messages);
+    }
+
+    [Test]
+    public void Initialize_LoadsRecentMessagesFromStorage()
+    {
+        this.SetupStorageMessages(this._now.AddMinutes(-30));
+
+        this._sut.Initialize();
+
+        Assert.That(this._sut.GetLastHourMessagesCount(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Initialize_IgnoresOldMessagesFromStorage()
+    {
+        this.SetupStorageMessages(this._now.AddHours(-2));
+
+        this._sut.Initialize();
+
+        Assert.That(this._sut.GetLastHourMessagesCount(), Is.Zero);
+    }
+
+    [Test]
+    public void Initialize_TracksNewMessagesFromEventBus()
+    {
+        this._sut.Initialize();
+
+        this.SimulateMessageReceived(this._now.AddSeconds(-10));
+
+        Assert.That(this._sut.GetLastHourMessagesCount(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void GetLastHourMessages_ReturnsRecentMessages()
+    {
+        this.SetupStorageMessages(this._now.AddMinutes(-30), this._now.AddMinutes(-45));
+        this._sut.Initialize();
+
+        var result = this._sut.GetLastHourMessages();
+
+        Assert.That(result, Has.Length.EqualTo(2));
+    }
+
+    [Test]
+    public void GetLastHourMessages_ExcludesOutdatedMessagesAfterTimePass()
+    {
+        this.SetupStorageMessages(this._now.AddMinutes(-50));
+        this._sut.Initialize();
+
+        this._now = this._now.AddMinutes(20);
+
+        Assert.That(this._sut.GetLastHourMessagesCount(), Is.Zero);
+    }
+
+    [Test]
+    public void GetHalfLastHourMessages_ReturnsOnlyLast30Minutes()
+    {
+        this.SetupStorageMessages(this._now.AddMinutes(-15), this._now.AddMinutes(-45));
+        this._sut.Initialize();
+
+        var result = this._sut.GetHalfLastHourMessages();
+
+        Assert.That(result, Has.Length.EqualTo(1));
+    }
+
+    [Test]
+    public void GetHalfLastHourMessagesCount_ReturnsCorrectCount()
+    {
+        this.SetupStorageMessages(this._now.AddMinutes(-10), this._now.AddMinutes(-20), this._now.AddMinutes(-40));
+        this._sut.Initialize();
+
+        var result = this._sut.GetHalfLastHourMessagesCount();
+
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
