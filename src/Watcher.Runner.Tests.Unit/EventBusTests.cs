e32d8784bc5994337585fd9d54a6e692c5f6037d
Marcin99b <marcin.bigos99@gmail.com>
2025-12-15 12:44:53 +0100

    empty test

--- src/Watcher.Runner.Tests.Unit/EventBusTests.cs
+++ src/Watcher.Runner.Tests.Unit/EventBusTests.cs
@@ -1,6 +1,14 @@
-﻿namespace Watcher.Runner.Tests.Unit;
+﻿using Watcher.Runner.DiscordEventHandlers;
+
+namespace Watcher.Runner.Tests.Unit;
 
 [TestFixture]
 public class EventBusTests
 {
+    [Test]
+    public void Test1()
+    {
+        var bus = new EventBus();
+        bus.Subscribe<MessageInfoReceivedEvent>(x => { });
+    }
 }
