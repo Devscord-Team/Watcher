1f4ef9a75d3652f1ac66acc637c3150880117fa7
Marcin99b <marcin.bigos99@gmail.com>
2025-12-16 19:50:46 +0100

    fix test

--- src/Watcher.Runner.Tests.Unit/RealTimeActivityMonitorTests.cs
+++ src/Watcher.Runner.Tests.Unit/RealTimeActivityMonitorTests.cs
@@ -26,7 +26,7 @@ public class RealTimeActivityMonitorTests
 
         this._now = new DateTime(2024, 6, 15, 12, 0, 0, DateTimeKind.Utc);
         _ = this._dateTimeProviderMock.Setup(x => x.GetUtcNow()).Returns(() => this._now);
-        _ = this._messagesStorageMock.Setup(x => x.GetMessagesInfos(It.IsAny<ulong?>(), It.IsAny<ulong?>(), It.IsAny<DateTime?>())).Returns([]);
+        _ = this._messagesStorageMock.Setup(x => x.GetMessagesInfos(It.IsAny<ulong?>(), It.IsAny<ulong?>(), It.IsAny<DateTime?>())).ReturnsAsync([]);
 
         _ = this._eventBusMock
             .Setup(x => x.Subscribe(It.IsAny<Action<MessageInfoReceivedEvent>>()))
