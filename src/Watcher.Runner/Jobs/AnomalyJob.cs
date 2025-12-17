c23c24576fb959f8fe25e68983f692c14f7c4cf4
Marcin99b <marcin.bigos99@gmail.com>
2025-12-17 17:25:12 +0100

    anomaly job

---
+++ src/Watcher.Runner/Jobs/AnomalyJob.cs
@@ -0,0 +1,12 @@
+﻿using Watcher.Runner.Domain.AnomalyDetection;
+
+namespace Watcher.Runner.Jobs
+{
+    public class AnomalyJob(IAnomalyDetector anomalyDetector)
+    {
+        public async Task Run()
+        {
+            //TODO
+        }
+    }
+}
